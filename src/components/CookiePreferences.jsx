61484e242b75d1af1f746935e2aae55aad2db8da
Prakhar Pathak <prakharpathak9450@gmail.com>
2024-02-18 22:18:22 +0530

    Fixed bugs

--- src/components/CookiePreferences.jsx
+++ src/components/CookiePreferences.jsx
@@ -3,8 +3,8 @@ import cookieLogo from "../assets/cookieLogo.png";
 export function CookiePreferences() {
   return (
     <div>
-      <button className="fixed bottom-0 right-0">
-        <img src={cookieLogo} width={40} height={40} />
+      <button className="fixed bottom-0 right-0 m-3">
+        <img src={cookieLogo} width={40} />
       </button>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,70 +2,72 @@ import logo from "../assets/logo.jpg";
 
 export function Navbar() {
   return (
-    <div className="bg-meta-gray flex justify-around mx-40 items-center navbar sticky top-10">
-      <div className="mx-5">
-        <img src={logo} width={200}></img>
-      </div>
-      <div className="flex justify-around items-center">
-        <div className="flex">
-          Features
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-6 ml-1"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m19.5 8.25-7.5 7.5-7.5-7.5"
-            />
-          </svg>
+    <div className="bg-meta-gray sticky top-10">
+      <div className="flex justify-around mx-40 items-center navbar">
+        <div className="mx-5">
+          <img src={logo} width={200}></img>
         </div>
-        <div className="ml-10 flex">
-          Build
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-6 ml-1"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m19.5 8.25-7.5 7.5-7.5-7.5"
-            />
-          </svg>
+        <div className="flex justify-around items-center">
+          <div className="flex">
+            Features
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-6 ml-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div>
+          <div className="ml-10 flex">
+            Build
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-6 ml-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div>
+          <div className="ml-10 flex">
+            Resources
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-6 ml-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div>
+          <div className="flex ml-10 mr-4">Learn</div>
+          <button className="bg-meta-blue rounded-3xl p-2 px-7">
+            View Portfolio
+          </button>
+          <label class="inline-flex items-center cursor-pointer m-7">
+            <input type="checkbox" value="" class="sr-only peer" />
+            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-600"></div>
+          </label>
         </div>
-        <div className="ml-10 flex">
-          Resources
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-6 ml-1"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m19.5 8.25-7.5 7.5-7.5-7.5"
-            />
-          </svg>
-        </div>
-        <div className="flex ml-10 mr-4">Learn</div>
-        <button className="bg-meta-blue rounded-3xl p-2 px-7">
-          View Portfolio
-        </button>
-        <label class="inline-flex items-center cursor-pointer m-7">
-          <input type="checkbox" value="" class="sr-only peer" />
-          <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-600"></div>
-        </label>
       </div>
     </div>
   );
